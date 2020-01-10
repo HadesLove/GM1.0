ce1eb712eb3af55b22e648271bab0f07f5dd25ce
hadesTso <caohades1996@outlook.com>
2020-01-10 18:10:51 +0800

    资源excel

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,6 @@ Route::any('device/activation', 'AjaxController@DeviceActivation');
 Route::any('check/activation', 'AjaxController@checkActivation');
 Route::any('callback', 'AjaxController@callback');
 Route::any('role/data', 'DataController@roleData');
-Route::any('resource/excel', 'DataController@resourceExcel');
 
 Route::post('time/tack1', 'GameController@timeTack');
 
@@ -96,6 +95,7 @@ Route::group(['middleware' => 'AuthToken', 'prefix' => 'auth'], function (){
     Route::any('child/list', 'DataController@childList');
     Route::any('role/stream/list', 'DataController@roleStreamList');
     Route::any('resource/list', 'DataController@resourceList');
+    Route::any('resource/excel', 'DataController@resourceExcel');
     Route::any('chat/list', 'DataController@chatList');
     Route::any('real/time/chat', 'DataController@RealTimeChat');
     Route::any('order/list', 'DataController@OrderList');
